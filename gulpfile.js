a3dd2b34238222b1e920df59f4b842513ea2fa59
Nick Downs <NickDowns55@gmail.com>
2017-09-10 12:47:17 -0700

    new gulp file task
    base gulp install

--- gulpfile.js
+++ gulpfile.js
@@ -1,102 +1,45 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
-var cssnano = require('gulp-cssnano');
-var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
-var del = require('del');
-var runSequence = require('run-sequence');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
 
-// Basic Gulp task syntax
-gulp.task('hello', function() {
-  console.log('Hello Zell!');
-})
 
-// Development Tasks 
-// -----------------
+// Set your full base url's here
+var devUrl = 'localhost:8888/standardPipes/'; 
+var fileUrl = 'sites/all/themes/sproutChild/'
+
 
 // Start browserSync server
 gulp.task('browserSync', function() {
   browserSync({
-    server: {
-      baseDir: ''
-    }
-  })
+    proxy: devUrl
+    })
 })
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
-    .pipe(gulp.dest('app/css')) // Outputs it in the css folder
-    .pipe(browserSync.reload({ // Reloading with Browser Sync
+  return gulp.src(fileUrl + 'sass/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sass())
+    .pipe(gulp.dest(fileUrl + 'css'))
+    .pipe(browserSync.reload({
       stream: true
-    }));
+    }))
 })
 
-// Watchers
-gulp.task('watch', function() {
-  gulp.watch('app/scss/**/*.scss', ['sass']);
-  gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('app/js/**/*.js', browserSync.reload);
-})
-
-// Optimization Tasks 
-// ------------------
-
-// Optimizing CSS and JavaScript 
-gulp.task('useref', function() {
-
-  return gulp.src('app/*.html')
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('dist'));
+// css minify
+gulp.task('minify', function(){
+	gulp.src(fileUrl + 'css/**/*.css')
+	    .pipe(minifyCSS())
+	    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+	    .pipe(concat('style.min.css'))
+	    .pipe(gulp.dest(fileUrl + 'css'))
 });
 
-// Optimizing Images 
-gulp.task('images', function() {
-  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-    // Caching images that ran through imagemin
-    .pipe(cache(imagemin({
-      interlaced: true,
-    })))
-    .pipe(gulp.dest('dist/images'))
-});
-
-// Copying fonts 
-gulp.task('fonts', function() {
-  return gulp.src('app/fonts/**/*')
-    .pipe(gulp.dest('dist/fonts'))
-})
-
-// Cleaning 
-gulp.task('clean', function() {
-  return del.sync('dist').then(function(cb) {
-    return cache.clearAll(cb);
-  });
-})
-
-gulp.task('clean:dist', function() {
-  return del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']);
-});
-
-// Build Sequences
-// ---------------
-
-gulp.task('default', function(callback) {
-  runSequence(['sass', 'browserSync'], 'watch',
-    callback
-  )
-})
-
-gulp.task('build', function(callback) {
-  runSequence(
-    'clean:dist',
-    'sass',
-    ['useref', 'images', 'fonts'],
-    callback
-  )
+gulp.task('watch', ['browserSync'], function (){
+  gulp.watch(fileUrl + 'sass/**/*.scss', ['sass','minify']).on('change', browserSync.reload);
+  // Other watchers
+  gulp.watch(fileUrl + '*.html', browserSync.reload);
+  gulp.watch(fileUrl + '*.php', browserSync.reload);
+  gulp.watch(fileUrl + 'js/**/*.js', browserSync.reload);
 })
